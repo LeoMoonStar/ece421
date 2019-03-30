6dbc8d3d42fc608a4b51b3f409b69a78a0b43beb
LisavB <44475388+LisavB@users.noreply.github.com>
2019-03-30 18:14:43 -0400

    Commit

--- a3/starter_gmm.py
+++ a3/starter_gmm.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
+import math
 
 # Loading data
 #data = np.load('data100D.npy')
@@ -20,18 +21,18 @@ if is_valid:
     val_data = data[rnd_idx[:valid_batch]]
     data = data[rnd_idx[valid_batch:]]
 
-def convariance_Mat(X, m):
+print(num_pts, dim)
+
+def convariance_Mat(X, mean):
     # Inputs
     # X: is an NxD matrix
     # m: some of the
 
-    x = tf.math.subtract(X, m)
-    print(x)
-    mean = tf.reduce_mean(x, axis=0, keep_dims=True)
-    cov = tf.matmul(tf.transpose(mean), mean)
-
-    return cov
-
+    #mean = tf.reduce_mean(X, axis=0, keep_dims=True)
+    # x_aux = tf.subtract(X, mean)
+    # cov = tf.matmul(x_aux, x_aux, transpose_a=True)/(x_aux.shape[0])
+    # return cov
+    return
 
 # Distance function for GMM
 def distanceFunc(X, MU):
@@ -41,41 +42,46 @@ def distanceFunc(X, MU):
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
 
-    # Make a covariance matrix
-    cov = convariance_Mat(X, m)
 
     # Find all the means
     #
 
     #means = tf.reshape(tf.reduce_sum(tf.square(X), axis=1), [-1, 1])/
-    #t1 = -2 * tf.matmul(X, MU, transpose_a=False, transpose_b=True)
-    #sqX = tf.reshape(tf.reduce_sum(tf.square(X), axis=1), [-1, 1])
-    #sqMU = tf.reshape(tf.reduce_sum(tf.square(MU), axis=1), [1, -1])
+    t1 = -2 * tf.matmul(X, MU, transpose_a=False, transpose_b=True)
+    sqX = tf.reshape(tf.reduce_sum(tf.square(X), axis=1), [-1, 1])
+    sqMU = tf.reshape(tf.reduce_sum(tf.square(MU), axis=1), [1, -1])
 
-    #ret = t1 + sqX + sqMU
-    #return ret
+    ret = t1 + sqX + sqMU
+    return ret
 
-#def log_GaussPDF(X, mu, sigma):
+def log_GaussPDF(X, mu, sigma):
     # Inputs
     # X: N X D
     # mu: K X D
     # sigma: K X 1
-    # log_pi: K X 1
-
+    #
     # Outputs:
     # log Gaussian PDF N X K
-
+    #
     # TODO
+    pairwise_dist = distanceFunc( X, mu )
+    sigma_dist = -1*tf.divide(pairwise_dist, 2*sigma)
+    coeff = -1*tf.log(2*math.pi*sigma)
 
-#def log_posterior(log_PDF, log_pi):
+    return coeff + sigma_dist
+
+def log_posterior(log_PDF, log_pi):
     # Input
     # log_PDF: log Gaussian PDF N X K
     # log_pi: K X 1
-
+    #
     # Outputs
     # log_post: N X K
-
+    #
     # TODO
+    p_xz = tf.add( log_PDF, log_pi )
+
+    return logsoftmax(p_xz)
 
 
 mat = [[1, 1], [1, 1]]
